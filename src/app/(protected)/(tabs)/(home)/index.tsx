e891785216fb85d9ae26959bce5883402330a4ea
Nenad <nenadmobmail@gmail.com>
2025-07-23 21:49:33 +0200

    expo api route test

--- src/app/(protected)/(tabs)/(home)/index.tsx
+++ src/app/(protected)/(tabs)/(home)/index.tsx
@@ -1,11 +1,19 @@
 import FeedPostItem from "@/components/FeedPostItem";
-import { FlatList, Pressable } from "react-native";
+import { Button, FlatList, Pressable } from "react-native";
 import dummyPosts from "@/dummyPosts";
 import { Link } from "expo-router";
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 
 export default function FeedScreen() {
+  const fetchApi = async () => {
+    const response = await fetch("/hello");
+    const json = await response.json();
+    console.log("Response from cliend side request: ", json);
+    console.log("From client side: ", process.env.SECRET_KEY);
+    console.log("From client side: ", process.env.EXPO_PUBLIC_SHARED_KEY);
+  };
+
   return (
     <>
       <FlatList
@@ -21,6 +29,8 @@ export default function FeedScreen() {
           <AntDesign name="plus" size={24} color="white" />
         </Pressable>
       </Link>
+
+      <Button onPress={fetchApi} title="Fetch API" />
     </>
   );
 }
